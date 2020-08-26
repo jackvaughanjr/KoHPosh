22e97ba445dbfdfcba404aff8c951b5fcf630296
Jack Vaughan, Jr <jvaughan@connectria.com>
2020-08-26 15:04:17 -0500

    Remove png files and replace with regular text

--- server.ps1
+++ server.ps1
@@ -45,27 +45,10 @@ Start-PodeServer {
                 "deaf" = $member.deaf
                 "roles" = $roleList
             }
-            If ($output.roles -contains 'Mob Level 4') { $output.mob = '<div class="mob04"></div>' }
-            If ($output.roles -contains 'Mob Level 5') { $output.mob = '<div class="mob05"></div>' }
-            If ($output.roles -contains 'Mob Level 6') { $output.mob = '<div class="mob06"></div>' }
-            If ($output.roles -contains 'Mob Level 7') { $output.mob = '<div class="mob07"></div>' }
-            If ($output.roles -contains 'Mob Level 8') { $output.mob = '<div class="mob08"></div>' }
-            If ($output.roles -contains 'Mob Level 9') { $output.mob = '<div class="mob09"></div>' }
-            If ($output.roles -contains 'Mob Level 10') { $output.mob = '<div class="mob10"></div>' }
-            If ($output.roles -contains 'Mob Level 11') { $output.mob = '<div class="mob11"></div>' }
-            If ($output.roles -contains 'Mob Level 12') { $output.mob = '<div class="mob12"></div>' }
-            If ($output.roles -contains 'TH 15') { $output.th = '<div class="th15"></div>' }
-            If ($output.roles -contains 'TH 16') { $output.th = '<div class="th16"></div>' }
-            If ($output.roles -contains 'TH 17') { $output.th = '<div class="th17"></div>' }
-            If ($output.roles -contains 'TH 18') { $output.th = '<div class="th18"></div>' }
-            If ($output.roles -contains 'TH 19') { $output.th = '<div class="th19"></div>' }
-            If ($output.roles -contains 'TH 20') { $output.th = '<div class="th20"></div>' }
-            If ($output.roles -contains 'TH 21') { $output.th = '<div class="th21"></div>' }
-            If ($output.roles -contains 'TH 22') { $output.th = '<div class="th22"></div>' }
-            If ($output.roles -contains 'TH 23') { $output.th = '<div class="th23"></div>' }
-            If ($output.roles -contains 'TH 24') { $output.th = '<div class="th24"></div>' }
-            If ($output.roles -contains 'TH 25') { $output.th = '<div class="th25"></div>' }
-            If ($output.roles -contains 'TH 26') { $output.th = '<div class="th26"></div>' }
+            If ($output.roles -like 'Mob Level*') { $output.mob = $($output.roles -like 'Mob Level*').Substring(10, $($output.roles -like 'Mob Level*').Length - 10) }
+            Else { $output.mob = '-' }
+            If ($output.roles -like 'TH*') { $output.th = $($output.roles -like 'TH*').Substring(3, 2) }
+            Else { $output.th = '-' }
             If ($output.roles -contains 'Biome - Grasslands') { $output.biome = '<div class="grassy"></div>' }
             If ($output.roles -contains 'Biome - Badlands') { $output.biome = '<div class="baddy"></div>' }
             If ($output.roles -contains 'Biome - Swamp') { $output.biome = '<div class="swampy"></div>' }
